925248ec6063a0923af92b04badab8a14ba165ec
Luis Tomasini <100858288+luistomasini@users.noreply.github.com>
2022-09-27 22:41:34 -0300

    continuacao estudos

---
+++ hcode-js/arquivos-udemy/calculadora/c03/scripts/controller/CalcController.js
@@ -0,0 +1,34 @@
+class CalcController {
+
+    constructor(){
+
+        this._displayCalc = "0";
+        this._dataAtual;
+
+    }
+
+    get displayCalc(){
+
+        return this._displayCalc;
+
+    }
+
+    set displayCalc(valor){
+
+        this._displayCalc = valor;
+
+    }
+
+    get dataAtual(){
+
+        return this._dataAtual;
+
+    }
+
+    set dataAtual(valor){
+
+        this._dataAtual = valor;
+
+    }
+
+}
